01ac8b75e80b4167765ddf5febd66e67152d5372
quocanhxemer <75298377+quocanhxemer@users.noreply.github.com>
2020-12-01 19:40:03 +0700

    Create exercise7.7.cpp

---
+++ exercise7.7.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Constant macros */
+#define CBEGIN 10
+#define CLIMIT -5
+#define CSTEP 5
+
+int main(void)
+{
+    /* Variable declarations */
+    int celsius;
+    double fahrenheit;
+
+    /* Print the table heading */
+    printf(" Celsius Fahrenheit\n");
+    for (celsius = CBEGIN;
+         celsius >= CLIMIT;
+         celsius = celsius - CSTEP)
+    {
+        fahrenheit = 1.8 * celsius + 32.0;
+        printf(" %3d %7.2f\n", celsius,
+               fahrenheit);
+    }
+
+    return (0);
+}
