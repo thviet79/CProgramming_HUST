7909fe8d686dc5f955bf02c67e1d3fd1dcba7697
quocanhxemer <75298377+quocanhxemer@users.noreply.github.com>
2020-12-10 16:32:08 +0700

    Create exercise27.c

---
+++ exercise27.c
@@ -0,0 +1,10 @@
+int main()
+{
+printf("#include <stdio.h>\n\n");
+printf("int main()\n");
+printf("{\n");
+printf(" printf(\"Pham Vuong Tu\\n\");\n");
+printf(" return 0;\n");
+printf("}\n");
+return 0;
+}
