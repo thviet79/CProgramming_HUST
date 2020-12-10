3542c4df7665c37cfc5ec2d8b017a5e6b9ec1cf1
quocanhxemer <75298377+quocanhxemer@users.noreply.github.com>
2020-12-10 16:29:40 +0700

    Update and rename exercise15.txt to exercise25.c

---
+++ exercise25.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main
+{
+  printf("****************************************");
+  printf("My name is Pham Nguyen Quoc Anh.\n");
+  printf("Nice to meet you.\n");
+  printf("Hope you will have happy time.\n");
+  printf("****************************************");
+}
