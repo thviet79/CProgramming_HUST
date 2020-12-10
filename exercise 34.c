d84119dfea54c274f2325618b7fa5f5fe0affb3d
quocanhxemer <75298377+quocanhxemer@users.noreply.github.com>
2020-12-10 16:40:06 +0700

    Create exercise 34.c

---
+++ exercise 34.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n, m, sum;
+printf("Enter first number: ");
+scanf("%d", &n);
+printf("Enter second number: ");
+scanf("%d", &m);
+sum = n+m;
+printf("Sum of %d and %d = %d\n", n, m, sum);
+}
