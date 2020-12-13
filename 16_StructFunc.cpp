1f27da05d5bdc84e3933f75c319ad05715067dc6
quocanhxemer <75298377+quocanhxemer@users.noreply.github.com>
2020-12-14 04:14:32 +0700

    Add files via upload

---
+++ 16_StructFunc.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student
+{
+    char Name[100];
+    int Gender;
+    double Grade;
+};
+
+void StudentIn(Student *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nSTUDENT %d\n", i + 1);
+        printf("Enter name:\n");
+        fflush(stdin);
+        fgets((p + i)->Name, 100, stdin);
+        printf("Enter Gender (1 = Male, 0 = Female):\n");
+        scanf("%d", &(p + i)->Gender);
+        printf("Enter Grade:\n");
+        scanf("%lf", &(p + i)->Grade);
+    }
+}
+
+int BestStudent(Student *p, int n)
+{
+    double max = p->Grade;
+    int best;
+    for (int i = 0; i < n; i++)
+    {
+        if (max <= (p + i)->Grade)
+        {
+            max = (p + i)->Grade;
+            best = i + 1;
+        }
+    }
+    return best;
+}
+
+void StudentOut(Student *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nSTUDENT %d\n", i + 1);
+        printf("Name: %s", (p + i)->Name);
+        printf("Gender: %s\n", (p + i)->Gender == 0 ? "Female" : (p + i)->Gender == 1 ? "Male" : "Error");
+        printf("Grade: %g", (p + i)->Grade);
+    }
+}
+
+int main()
+{
+    printf("Enter the number of students:\n");
+    int n;
+    scanf("%d", &n);
+    Student *p = (Student *)malloc(n * (sizeof(Student)));
+    StudentIn(p, n);
+    StudentOut(p, n);
+    printf("\nStudent %d has the best performance!", BestStudent(p, n));
+    free(p);
+}
